238ca453962422838a93d21f981ca8fb4ddd62fa
KiranDhawan <dhawankiran399@gmail.com>
2024-10-07 11:57:12 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,20 @@
+console.log ("here is he code")
+
+let box = document.querySelector(".container").children;
+
+
+
+function getRandomColor(){   
+    let val1 = Math.ceil( 0 + Math.random()*255);
+    let val2 = Math.ceil( 0 + Math.random()*255);
+    let val3 = Math.ceil( 0 + Math.random()*255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+    
+}   
+
+Array.from(box).forEach(e=> {
+    e.style.backgroundColor = getRandomColor();
+    e.style.color = getRandomColor();
+
+    
+})
